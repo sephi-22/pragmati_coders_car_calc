78ea2c870b3eaa198d22daa81b93d8795ab324ed
Prateek Shekhar <sephi-22@hotmail.com>
2023-12-07 23:33:40 +0400

    Added multiple text

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,4 @@
-from tkinter import *
-root = Tk()
-
 myLabel1 = Label(root, text = "Hello World")
 myLabel2 = Label(root, text = "My name is Prateek Shekhar")
 myLabel1.grid(row=0,column=1)
 myLabel2.grid(row=1,column=1)
-root.mainloop()
